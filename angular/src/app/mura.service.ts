cdc030039ebe390d490236730cd3d80318df948a
Matt Levine <matt.levine@blueriver.com>
2019-01-09 19:25:52 -0800

    Removing unneeded class

--- angular/src/app/mura.service.ts
+++ angular/src/app/mura.service.ts
@@ -23,20 +23,6 @@ export class MuraService {
 			.loadcss(Mura.corepath + '/modules/v1/core_assets/css/mura.7.1.min.css')
 			.loadcss(Mura.corepath + '/modules/v1/core_assets/css/mura.7.1.skin.css');
 
-
-		Mura.UI.Collection=Mura.UI.Collection.extend(
-		{
-			renderClient:function(){
-				if (typeof Mura.Module[this.context.layout] != 'undefined'){
-					this.getLayoutInstance().renderClient();
-				} else {
-					this.context.targetEl.innerHTML="This collection has an undefined layout";
-				}
-				this.trigger('afterRender');
-			}
-
-		});
-
 		const domServiceRef=this.domService;
 
 		Mura.UI.Angular=Mura.UI.extend(
